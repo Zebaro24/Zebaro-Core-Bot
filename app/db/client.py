f35dab127ec69b15e931f818be6e563096524604
Zebaro <denok100100@gmail.com>
2025-10-19 13:10:22 +0200

    Add a collection for GitHub

--- app/db/client.py
+++ app/db/client.py
@@ -5,6 +5,7 @@ client = AsyncMongoClient(settings.mongo_uri)
 db = client.get_database()
 
 jobs_collection = db["jobs"]
+github_notification_collection = db["github_notification"]
 
 async def start_db():
     await client.aconnect()
