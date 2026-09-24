38070687b5f763c8d6e6afd3275c010810ea3228
Zebaro <denok100100@gmail.com>
2026-09-24 22:29:28 +0200

    chore(release): v0.11.0

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     app_name: str = "Zebaro-Core-Bot"
     description: str = ""
     author: str = "Zebaro (zebaro.dev)"
-    version: str = "0.10.2"
+    version: str = "0.11.0"
 
     debug: bool = False
 
